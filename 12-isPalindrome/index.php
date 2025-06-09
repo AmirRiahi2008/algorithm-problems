60941a3f5df848f3d1aaa8c58b1a757822762fc6
Amirreza Riahi <amirreza.riahi3311@gmail.com>
2025-06-09 11:21:08 +0330

    Add 12-isPalindrome Algorithm

---
+++ 12-isPalindrome/index.php
@@ -0,0 +1,15 @@
+<?php
+class Solution
+{
+   public function isPalindrome($num) {
+    $str = strval($num);
+    $arr = str_split($str);
+    $reversed = implode('', array_reverse($arr));
+    return $str === $reversed;
+}
+
+}
+
+$solutionModel = new Solution();
+var_dump($solutionModel->isPalindrome(123)); // [1,2]
+var_dump($solutionModel->isPalindrome(121)); // [1,2,3]
