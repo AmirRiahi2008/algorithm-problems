c9b0f606a7d86bcc03f3fb386c5014e2eb6ffc16
Amirreza Riahi <amirreza.riahi3311@gmail.com>
2025-06-10 12:44:37 +0330

    update 12

--- 12-isPalindrome/index.php
+++ 12-isPalindrome/index.php
@@ -11,12 +11,7 @@ class Solution
 }
 
 $solutionModel = new Solution();
-<<<<<<< HEAD
-var_dump($solutionModel->isPalindrome(123)); // [1,2]
-var_dump($solutionModel->isPalindrome(10)); // [1,2,3]
-=======
 var_dump($solutionModel->isPalindrome(123)); // false
 var_dump($solutionModel->isPalindrome(121)); // true
 // Question
 // https://leetcode.com/problems/palindrome-number/
->>>>>>> 7cb6597ce95dbf5420da6f7b9582f66b54b8f6f7
