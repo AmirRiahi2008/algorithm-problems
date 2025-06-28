281fdb36a62edf11d4fa3f7630e13f51113333f1
Amirreza Riahi <amirreza.riahi3311@gmail.com>
2025-06-28 09:51:10 +0330

    Add 15-happy number

---
+++ 15-Happy Number/index.php
@@ -0,0 +1,24 @@
+<?php
+class Solution
+{
+    public function isHappy($n)
+    {
+        $seen = [];
+        while ($n !== 1) {
+            if (isset($seen[$n]))
+                return false;
+            $seen[$n] = true;
+            $digits = str_split((string) $n);
+            $sum = 0;
+            foreach ($digits as $digit) {
+                $sum += $digit ** 2;
+            }
+            $n = $sum;
+        }
+        return true;
+    }
+}
+
+$solutionModel = new Solution();
+var_dump($solutionModel->isHappy(19)); // true
+var_dump($solutionModel->isHappy(2));  // false
